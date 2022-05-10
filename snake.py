1fe6e7f70450f4bf0c085182e386919b75009029
Diggory2303 <dieguito.isunza@gmail.com>
2022-05-10 11:04:30 -0500

    Se agrego la funcionalidad que permite que la serpiente y la comida tengan colores random nunca iguales entre ellos en cada run

--- snake.py
+++ snake.py
@@ -6,6 +6,18 @@ food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
+#Creacion de lista de paleta de colores disponibles
+colors=['green','blue','yellow','orange','purple']
+#Numero aleatorio entre el 0 y el 4
+random_number = randrange(0,5)
+#Creacion del color del snake
+random_color_snake=colors[random_number]
+#Se quita el color del sanke en la lista para que no afecte a la comida
+colors.remove(colors[random_number])
+#Creacion del color de la comida
+random_color_food = colors[random_number]
+
+
 def change(x, y):
     "Change snake direction."
     aim.x = x
@@ -36,10 +48,12 @@ def move():
 
     clear()
 
-    for body in snake:
-        square(body.x, body.y, 9, 'black')
+    
 
-    square(food.x, food.y, 9, 'green')
+    for body in snake:
+        square(body.x, body.y, 9, random_color_snake)
+
+    square(food.x, food.y, 9, random_color_food)
     update()
     ontimer(move, 100)
 
